1dd8e1dc0951930137885e2d1ef696e7408d7f70
Kalen Jordan <kalenj@gmail.com>
2025-05-15 13:05:46 -0500

    Rename sheets-helpers.js to city-sheets-common.js and update import in backfill-google-sheets job.js to reflect the new file name

--- jobs/city/backfill-google-sheets/job.js
+++ jobs/city/backfill-google-sheets/job.js
@@ -2,7 +2,7 @@ import GetOrdersForBackfill from "../../../graphql/GetOrdersForBackfill.js";
 import * as GoogleSheets from "../../../connectors/google-sheets.js";
 import chalk from "chalk";
 import { logToCli, logToWorker } from "../../../utils/log.js";
-import * as SheetsHelpers from "../sheets-helpers.js";
+import * as SheetsHelpers from "../city-sheets-common.js";
 
 /**
  * Process an order backfill job
